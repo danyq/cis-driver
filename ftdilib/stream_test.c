6500ba9574b1ebc7611ccd472bbcf7676c82e7f9
Dany Qumsiyeh <dany@qhex.org>
2016-08-17 12:57:46 -0700

    libftdi example debug

--- ftdilib/stream_test.c
+++ ftdilib/stream_test.c
@@ -1,5 +1,7 @@
 /* stream_test.c
  *
+ * from libftdi examples
+ *
  * Test reading  from FT2232H in synchronous FIFO mode.
  *
  * The FT2232H must supply data due to an appropriate circuit
@@ -24,11 +26,9 @@
 #include <signal.h>
 #include <errno.h>
 #include <ftdi.h>
-void check_outfile(char *);
 
 static FILE *outputFile;
 
-static int check = 1;
 static int exitRequested = 0;
 /*
  * sigintHandler --
@@ -49,7 +49,6 @@ usage(const char *argv0)
            "Usage: %s [options...] \n"
            "Test streaming read from FT2232H\n"
            "[-P string] only look for product with given string\n"
-           "[-n] don't check for special block structure\n"
            "\n"
            "If some filename is given, write data read to that file\n"
            "Progess information is printed each second\n"
@@ -63,56 +62,17 @@ usage(const char *argv0)
    exit(1);
 }
 
-static uint32_t start = 0;
-static uint32_t offset = 0;
-static uint64_t blocks = 0;
-static uint32_t skips = 0;
+static int read_dbg = 1;
 static uint32_t n_err = 0;
 static int
 readCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress, void *userdata)
 {
+  if (read_dbg) {
+    fprintf(stderr, "read callback\n");
+    read_dbg = 0;
+  }
    if (length)
    {
-       if (check)
-       {
-           int i,rem;
-           uint32_t num;
-           for (i= offset; i<length-16; i+=16)
-           {
-               num = *(uint32_t*) (buffer+i);
-               if (start && (num != start +0x4000))
-               {
-                   uint32_t delta = ((num-start)/0x4000)-1;
-                   fprintf(stderr, "Skip %7d blocks from 0x%08x to 0x%08x at blocks %10llu\n",
-                           delta, start -0x4000, num, (unsigned long long)blocks);
-                   n_err++;
-                   skips += delta;
-               }
-               blocks ++;
-               start = num;
-           }
-           rem = length -i;
-           if (rem >3)
-           {
-               num = *(uint32_t*) (buffer+i);
-               if (start && (num != start +0x4000))
-               {
-                   uint32_t delta = ((num-start)/0x4000)-1;
-                   fprintf(stderr, "Skip %7d blocks from 0x%08x to 0x%08x at blocks %10llu\n",
-                           delta, start -0x4000, num, (unsigned long long) blocks);
-                   n_err++;
-                   skips += delta;
-               }
-               start = num;
-           }
-           else if (rem)
-               start += 0x4000;
-           if (rem != 0)
-           {
-               blocks ++;
-               offset = 16-rem;
-           }
-       }
        if (outputFile)
        {
            if (fwrite(buffer, length, 1, outputFile) != 1)
@@ -154,9 +114,6 @@ int main(int argc, char **argv)
        case 'P':
            descstring = optarg;
            break;
-       case 'n':
-           check = 0;
-           break;
        default:
            usage(argv[0]);
        }
@@ -177,6 +134,7 @@ int main(int argc, char **argv)
        fprintf(stderr, "ftdi_new failed\n");
        return EXIT_FAILURE;
    }
+   fprintf(stderr, "ftdi_new ok\n");
    
    if (ftdi_set_interface(ftdi, INTERFACE_A) < 0)
    {
@@ -184,6 +142,7 @@ int main(int argc, char **argv)
        ftdi_free(ftdi);
        return EXIT_FAILURE;
    }
+   fprintf(stderr, "ftdi_set_interface ok\n");
    
    if (ftdi_usb_open_desc(ftdi, 0x0403, 0x6014, descstring, NULL) < 0)
    {
@@ -191,6 +150,7 @@ int main(int argc, char **argv)
        ftdi_free(ftdi);
        return EXIT_FAILURE;
    }
+   fprintf(stderr, "ftdi_usb_open ok\n");
    
    /* A timeout value of 1 results in may skipped blocks */
    if(ftdi_set_latency_timer(ftdi, 2))
@@ -200,6 +160,7 @@ int main(int argc, char **argv)
        ftdi_free(ftdi);
        return EXIT_FAILURE;
    }
+   fprintf(stderr, "ftdi_set_latency ok\n");
    
 /*   if(ftdi_usb_purge_rx_buffer(ftdi) < 0)
    {
@@ -234,125 +195,5 @@ int main(int argc, char **argv)
    ftdi_usb_close(ftdi);
    ftdi_free(ftdi);
    signal(SIGINT, SIG_DFL);
-   if (check && outfile)
-   {
-       if ((outputFile = fopen(outfile,"r")) == 0)
-       {
-           fprintf(stderr,"Can't open logfile %s, Error %s\n", outfile, strerror(errno));
-           ftdi_usb_close(ftdi);
-           ftdi_free(ftdi);
-           return EXIT_FAILURE;
-       }
-       check_outfile(descstring);
-       fclose(outputFile);
-   }
-   else if (check)
-       fprintf(stderr,"%d errors of %llu blocks (%Le), %d (%Le) blocks skipped\n",
-               n_err, (unsigned long long) blocks, (long double)n_err/(long double) blocks,
-               skips, (long double)skips/(long double) blocks);
    exit (0);
-}
-
-void check_outfile(char *descstring)
-{
-    if(strcmp(descstring,"FT2232HTEST") == 0)
-    {
-       char buf0[1024];
-       char buf1[1024];
-       char bufr[1024];
-       char *pa, *pb, *pc;
-       unsigned int num_lines = 0, line_num = 1;
-       int err_count = 0;
-       unsigned int num_start, num_end;
-
-       pa = buf0;
-       pb = buf1;
-       pc = buf0;
-       if(fgets(pa, 1023, outputFile) == NULL)
-       {
-           fprintf(stderr,"Empty output file\n");
-           return;
-       }
-       while(fgets(pb, 1023, outputFile) != NULL)
-       {
-           num_lines++;
-           unsigned int num_save = num_start;
-           if( sscanf(pa,"%6u%94s%6u",&num_start, bufr,&num_end) !=3)
-           {
-               fprintf(stdout,"Format doesn't match at line %8d \"%s",
-                       num_lines, pa);
-               err_count++;
-               line_num = num_save +2;
-           }
-           else
-           {
-               if ((num_start+1)%100000 != num_end)
-               {
-                   if (err_count < 20)
-                       fprintf(stdout,"Malformed line %d \"%s\"\n", 
-                               num_lines, pa);
-                   err_count++;
-               }
-               else if(num_start != line_num)
-               {
-                   if (err_count < 20)
-                       fprintf(stdout,"Skipping from %d to %d\n", 
-                               line_num, num_start);
-                   err_count++;
-                  
-               }
-               line_num = num_end;
-           }
-           pa = pb;
-           pb = pc;
-           pc = pa;
-       }
-       if(err_count)
-           fprintf(stdout,"\n%d errors of %d data sets %f\n", err_count, num_lines, (double) err_count/(double)num_lines);
-       else
-           fprintf(stdout,"No errors for %d lines\n",num_lines);
-   }
-    else if(strcmp(descstring,"LLBBC10") == 0)
-    { 
-        uint32_t block0[4];
-        uint32_t block1[4];
-        uint32_t *pa = block0;
-        uint32_t *pb = block1;
-        uint32_t *pc = block0;
-        uint32_t start= 0;
-        uint32_t nread = 0;
-        int n_shown = 0;
-        int n_errors = 0;
-        if (fread(pa, sizeof(uint32_t), 4,outputFile) < 4)
-        {
-            fprintf(stderr,"Empty result file\n");
-            return;
-        }
-        while(fread(pb, sizeof(uint32_t), 4,outputFile) != 0)
-        {
-            blocks++;
-            nread =  pa[0];
-            if(start>0 && (nread != start))
-            {
-                if(n_shown < 30)
-                {
-                    fprintf(stderr, "Skip %7d blocks from 0x%08x to 0x%08x at blocks %10llu \n",
-                            (nread-start)/0x4000, start -0x4000, nread, (unsigned long long) blocks);
-                    n_shown ++;
-                }
-                n_errors++;
-            }
-            else if (n_shown >0) 
-                n_shown--; 
-            start = nread + 0x4000;
-            pa = pb;
-            pb = pc;
-            pc = pa;
-        }
-        if(n_errors)
-            fprintf(stderr, "%d blocks wrong from %llu blocks read\n",
-                    n_errors, (unsigned long long) blocks);
-        else
-            fprintf(stderr, "%llu blocks all fine\n", (unsigned long long) blocks);
-    }
 }
